83803b1773a3e1883ebdd0d442e01eceb1b16787
imaneaity <imaneaity@gmail.com>
2022-07-27 11:34:43 +0100

    Ajout des fixtures et du controle d'acces IsGranted()

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -7,11 +7,16 @@ namespace App\Controller;
 use App\Entity\Book;
 use App\Form\AdminBookType;
 use App\Repository\BookRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+
+
+#[IsGranted('ROLE_ADMIN')]
 class BookController extends AbstractController
 {
     // Attacher une Route (OK)

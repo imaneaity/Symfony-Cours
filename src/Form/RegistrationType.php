3f368f81be308e27c57a2b1b1c2879e889077450
imaneaity <imaneaity@gmail.com>
2022-07-27 10:05:26 +0100

    Création d'utilisateurs et formulaire d'inscription

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType :: class,[
+                'label' => 'Email:',
+                'required' => true,
+            ])
+            
+            ->add('password', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe :',
+                ],
+                'second_options' => [
+                    'label' => 'Répéter votre mot de passe :',
+                ]
+
+            ])
+                ->add('send', SubmitType::class, [
+                    'label' => "S'inscrire",
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}

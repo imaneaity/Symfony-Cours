e7e4c69b5e4af80072fed32d2c739b4044fd36be
imaneaity <imaneaity@gmail.com>
2022-07-29 09:32:39 +0200

    Correction de l'exercice du panier,création des formulaires address et payment, validation commande

---
+++ src/Form/PaymentType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\DTO\Payment;
+use App\Form\AddressType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PaymentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('cardNumber',TextType::class, [
+                'label' => 'Numéro de carte bleu :',
+                'required' => true,])
+
+            ->add('cardName',TextType::class, [
+                'label' => 'Nom et prénom :',
+                'required' => true,])
+            ->add('expirationDate', TextType::class, [
+                'label' => "date d'expiration :",
+                'required' => true,])
+            ->add('cvc',TextType::class, [
+                'label' => 'Code secret (CVC) :',
+                'required' => true,])
+            ->add('address', AddressType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Payment::class,
+        ]);
+    }
+}
